6d8e5f49a10b5fa482ef5115d60a1406ff1d1c95
howard-at-hp <howard.kim@healingpaper.com>
2025-01-29 15:25:17 +0900

    Grpc 서비스를 구현한다

--- src/main/kotlin/org/example/testgrpc/TestGrpcApplication.kt
+++ src/main/kotlin/org/example/testgrpc/TestGrpcApplication.kt
@@ -1,11 +1,11 @@
 package org.example.testgrpc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["org.example"])
 class TestGrpcApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/org/example/testgrpc/api/grpc/GrpcGreeting.kt
+++ src/main/kotlin/org/example/testgrpc/api/grpc/GrpcGreeting.kt
@@ -1,18 +1,15 @@
 package org.example.testgrpc.api.grpc
 
 import io.grpc.stub.StreamObserver
-import org.example.testgrpc.contracts.proto.GreetingProto
-import org.example.testgrpc.contracts.proto.GreetingServiceGrpc
-import org.example.testgrpc.contracts.response.Greeting
-import org.springframework.grpc.server.service.GrpcService
+import org.springframework.stereotype.Service
 
-@GrpcService
+@Service
 class GrpcGreeting : GreetingServiceGrpc.GreetingServiceImplBase() {
 
     override fun greeting(
         query: GreetingProto.GreetingApiQuery,
         responseObserver: StreamObserver<GreetingProto.GreetingResponse>,
-    )  {
+    ) {
         val builder = GreetingProto.GreetingResponse.newBuilder()
         builder.setMessage("Welcome, ${query.name}")
         val response = builder.build()
